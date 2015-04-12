4ffc0525140955a7d8d94c7be8bb734993194b96
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2015-04-12 00:50:30 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup 
 
-setup(name='backoff', 
-      version='1.0', 
+setup(name='exbackoff',
+      version='1.1.1',
       description='Decorator-based exponential backoff',
       author='Roberto Aguilar', 
-      author_email='roberto@baremetal.io', 
+      author_email='roberto@zymbit.com',
       py_modules=['backoff'], 
       long_description=open('README.md').read(),
       url='http://github.com/rca/backoff',
